4aefe83e2aa53e12feab0dce1fd7150c3492cac8
Omar Korim <omarkorim3210@gmail.com>
2023-09-26 17:45:09 +0300

    Add files via upload

---
+++ niceIOS2/niceIOS2/niceIOS2App.swift
@@ -0,0 +1,17 @@
+//
+//  niceIOS2App.swift
+//  niceIOS2
+//
+//  Created by Omar Mohamed Korim on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct niceIOS2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
